fd8b3074f9e9106544de04bb5d7b827e8e1f33dd
Calebe Oliveira <calebe81899026@gmail.com>
2025-03-30 20:59:24 -0300

    update show techs

--- src/app/show-techs.tsx
+++ src/app/show-techs.tsx
@@ -1,50 +1,90 @@
-import { Image, StyleSheet, View } from "react-native";
+import { Image, ScrollView, StyleSheet, View, Text } from "react-native";
 
 export default function ShowTechs() {
   return (
     <View style={styles.container}>
-      <View>
-        <Image
-          style={styles.icon}
-          source={require("../../assets/images/bootstrap.png")}
-        />
-      </View>
-      <Image
-        style={styles.icon}
-        source={require("../../assets/images/react.png")}
-      />
-      <Image
-        style={styles.icon}
-        source={require("../../assets/images/electron.png")}
-      />
-      <Image
-        style={styles.icon}
-        source={require("../../assets/images/css.png")}
-      />
-      <Image
-        style={styles.icon}
-        source={require("../../assets/images/html.png")}
-      />
-      <Image
-        style={styles.icon}
-        source={require("../../assets/images/javascript.png")}
-      />
-      <Image
-        style={styles.icon}
-        source={require("../../assets/images/jquery.png")}
-      />
-      <Image
-        style={styles.icon}
-        source={require("../../assets/images/mongo.png")}
-      />
-      <Image
-        style={styles.icon}
-        source={require("../../assets/images/firebase.png")}
-      />
-      <Image
-        style={styles.icon}
-        source={require("../../assets/images/node.png")}
-      />
+      <ScrollView>
+        <View style={styles.techView}>
+          <Image
+            style={styles.icon}
+            source={require("../../assets/images/bootstrap.png")}
+          />
+
+          <Text style={styles.techViewText}>Bootstrap</Text>
+        </View>
+        <View style={styles.techView}>
+          <Image
+            style={styles.icon}
+            source={require("../../assets/images/react.png")}
+          />
+
+          <Text style={styles.techViewText}>React</Text>
+        </View>
+        <View style={styles.techView}>
+          <Image
+            style={styles.icon}
+            source={require("../../assets/images/electron.png")}
+          />
+
+          <Text style={styles.techViewText}>Electron</Text>
+        </View>
+        <View style={styles.techView}>
+          <Image
+            style={styles.icon}
+            source={require("../../assets/images/css.png")}
+          />
+
+          <Text style={styles.techViewText}>CSS</Text>
+        </View>
+        <View style={styles.techView}>
+          <Image
+            style={styles.icon}
+            source={require("../../assets/images/html.png")}
+          />
+
+          <Text style={styles.techViewText}>HTML</Text>
+        </View>
+        <View style={styles.techView}>
+          <Image
+            style={styles.icon}
+            source={require("../../assets/images/javascript.png")}
+          />
+
+          <Text style={styles.techViewText}>JavaScript</Text>
+        </View>
+        <View style={styles.techView}>
+          <Image
+            style={styles.icon}
+            source={require("../../assets/images/jquery.png")}
+          />
+
+          <Text style={styles.techViewText}>Jquery</Text>
+        </View>
+        <View style={styles.techView}>
+          <Image
+            style={styles.icon}
+            source={require("../../assets/images/mongo.png")}
+          />
+
+          <Text style={styles.techViewText}>Mongo</Text>
+        </View>
+        <View style={styles.techView}>
+          <Image
+            style={styles.icon}
+            source={require("../../assets/images/firebase.png")}
+          />
+
+          <Text style={styles.techViewText}>Firebase</Text>
+        </View>
+        <View style={styles.techView}>
+          <Image
+            style={styles.icon}
+            source={require("../../assets/images/node.png")}
+          />
+
+          <Text style={styles.techViewText}>Node</Text>
+        </View>
+      </ScrollView>
     </View>
   );
 }
@@ -53,6 +93,21 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: "#121214",
     height: "100%",
+    paddingTop: 50,
+    paddingBottom: 50,
+  },
+
+  techView: {
+    alignItems: "center",
+    justifyContent: "space-between",
+    flexDirection: "row",
+    width: "60%",
+  },
+
+  techViewText: {
+    color: "#ddd",
+    fontSize: 16,
+    textAlign: "left",
   },
 
   icon: {
